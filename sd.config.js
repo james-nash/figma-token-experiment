ab1f20dc5f4608029394f06b2bebb5b0a01626e5
James Nash <james.nash@jpmorgan.com>
2022-04-13 17:03:23 +0100

    added initial Style Dictionary CSS export

---
+++ sd.config.js
@@ -0,0 +1,22 @@
+/*
+  Style Dictionary config
+*/
+module.exports = {
+  source: ['tmp/**/*.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'dist/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true
+          }
+        }
+      ]
+    }
+  }
+
+};
